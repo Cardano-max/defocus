446a3730b3d9da5610a70ae30d32aea362024e38
Hasnain Ali <hasnain.ali@A006-01314.local>
2024-07-10 11:54:40 +0500

    changed target size

--- webui2.py
+++ webui2.py
@@ -38,7 +38,7 @@ def virtual_try_on(clothes_image, person_image):
         clothes_image = HWC3(clothes_image)
         person_image = HWC3(person_image)
 
-        target_size = (1152, 896)
+        target_size = (512, 512)
         clothes_image = resize_image(clothes_image, target_size[0], target_size[1])
         person_image = resize_image(person_image, target_size[0], target_size[1])
 
