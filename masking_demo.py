b8ba8328152f6ff287d4c07fa0390a872e95cd22
ateeb <mateeb.ce41ceme@ce.ceme.edu.pk>
2024-07-29 22:39:56 +0500

    Your commit message

--- masking_demo.py
+++ masking_demo.py
@@ -95,7 +95,7 @@ class Masking:
 
 if __name__ == "__main__":
     masker = Masking()
-    human_img = Image.open("images/ok.png").convert('RGB')
+    human_img = Image.open("images/mota.jpg").convert('RGB')
     mask = masker.get_mask(human_img, category='upper_body')
     
     # Convert the mask to OpenCV format
