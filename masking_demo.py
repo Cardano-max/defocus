28a44631f24e6633cc5a4e26ba6a9610978873d2
ateeb <mateeb.ce41ceme@ce.ceme.edu.pk>
2024-07-31 12:40:38 +0500

    Masking DEMO

--- masking_demo.py
+++ masking_demo.py
@@ -165,7 +165,7 @@ from Masking.masking import Masking
 if __name__ == "__main__":
     masker = Masking()
     image_folder = "/Users/ikramali/projects/arbiosft_products/arbi-tryon/TEST"
-    input_image = os.path.join(image_folder, "women.jpeg")
+    input_image = os.path.join(image_folder, "GIRL.jpeg")
     output_mask = os.path.join(image_folder, "output_smooth_mask.png")
     output_masked = os.path.join(image_folder, "output_masked_image.png")
     category = "dresses"  # Change this to "upper_body", "lower_body", or "dresses" as needed
